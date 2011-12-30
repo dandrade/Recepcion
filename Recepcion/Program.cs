2ad89aa57afba2bd021fb8b6f6e94fb616437fa1
Daniel Andrade <dam@unixmexico.org>
2011-12-30 15:42:31 -0600

    Release to GitHub

--- Recepcion/Program.cs
+++ Recepcion/Program.cs
@@ -15,7 +15,7 @@ namespace Recepcion
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new Identificacion());
         }
     }
 }
